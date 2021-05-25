5f4faae5468a4bbd90fd2b0b12835e5cc64e7967
二宁 <12345678>
2021-05-25 15:39:48 +0800

    托盘区添加右键菜单

--- trip/ListWindow.xaml.cs
+++ trip/ListWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Windows;
 using System.Windows.Forms;
@@ -21,7 +20,6 @@ namespace trip
         public ListWindow()
         {
             InitializeComponent();
-            InitialTray(); //一启动就最小化至托盘
 
             if (AdminUtil.IsRunAsAdmin())
                 RegUtil.SelfRunning();
@@ -43,13 +41,20 @@ namespace trip
                     mainWindow.Show();
                 }
             }
+
+            InitialTray(); //一启动就最小化至托盘
         }
 
         private void MenuItem_Click_Show(object sender, RoutedEventArgs e)
         {
             int index = listView.SelectedIndex;
             Trip item = (Trip)listView.Items[index];
+            ShowItemWindow(item);
+        }
 
+        // 显示某一个贴士的窗口
+        private void ShowItemWindow(Trip item) 
+        {
             MainWindow mainWindow = null;
             foreach (MainWindow mainWindow1 in mainWindows)
             {
@@ -145,9 +150,11 @@ namespace trip
                 Visible = true,//托盘按钮是否可见
                 Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath)//托盘中显示的图标
             };
+            RefreshContextMenu();
+
             _notifyIcon.ShowBalloonTip(2000);//托盘气泡显示时间
             //_notifyIcon.MouseDoubleClick += notifyIcon_MouseDoubleClick;
-            _notifyIcon.MouseClick += notifyIcon_MouseDoubleClick;
+            _notifyIcon.MouseClick += NotifyIcon_MouseClick;
             //窗体状态改变时触发
             this.StateChanged += MainWindow_StateChanged;
         }
@@ -160,12 +167,13 @@ namespace trip
             {
                 this.WindowState = WindowState.Normal; // 取消最小化的操作
                 this.Visibility = Visibility.Hidden; // 隐藏窗口
+                RefreshContextMenu();// 添加右键菜单
             }
         }
         #endregion
 
         #region 托盘图标鼠标单击事件
-        private void notifyIcon_MouseDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        private void NotifyIcon_MouseClick(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
             {
@@ -179,7 +187,38 @@ namespace trip
                     this.Activate();
                 }
             }
+            else if (e.Button == MouseButtons.Right) 
+            {
+            }
         }
         #endregion
+
+        // 刷新右键菜单项
+        private void RefreshContextMenu()
+        {
+            List<MenuItem> menuItems = new List<MenuItem>();
+            for(int i= 0;i< listView.Items.Count;i++)
+            {
+                Trip trip = (Trip)listView.Items[i];
+                string text = trip.Content.Trim().Replace("\n", "").Replace("\r", "").Replace("\r\n", "");
+                if (text.Length > 20)
+                {
+                    text = text.Substring(0, 20);
+                }
+                MenuItem menuItem = new MenuItem(text);
+                menuItem.Tag = trip;
+                menuItem.Click += new EventHandler(OnClickContextMentItem);
+                menuItems.Add(menuItem);
+            }
+            _notifyIcon.ContextMenu = new ContextMenu(menuItems.ToArray());
+        }
+
+        // 点击了右键菜单中的某一项
+        private void OnClickContextMentItem(object sender, EventArgs e)
+        {
+            MenuItem menuItem = (MenuItem)sender;
+            Trip trip = (Trip)menuItem.Tag;
+            ShowItemWindow(trip);
+        }
     }
 }
